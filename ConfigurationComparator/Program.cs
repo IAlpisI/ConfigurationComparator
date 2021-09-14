58ab59c4c581afdd3fe3f9a9dab96b7ba933f903
alpis <57991374+IAlpisI@users.noreply.github.com>
2021-09-14 08:05:34 +0300

    Add project files.

---
+++ ConfigurationComparator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConfigurationComparator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
